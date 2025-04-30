7625bc67c44fc8b5c1600e8f48a1f868dbc0f30e
Camber Griffin <cambergriffin@Cambers-MBP.attlocal.net>
2025-04-30 19:04:25 -0400

    remove deploy docs for next go of it

--- terraform/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "app_instance_public_ip" {
-  description = "The public IP of the EC2 instance running the app"
-  value       = aws_instance.app.public_ip
-}
-
-output "db_endpoint" {
-  description = "The endpoint of the RDS PostgreSQL database"
-  value       = aws_db_instance.postgres.endpoint
-}
